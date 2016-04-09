03080b8f4189e527c5b6d7493e27c3070a481502
JonathanKross <kross.jonathan+github@gmail.com>
2016-04-09 19:43:33 -0400

    First Interface feature working. Can display top rated movies by mininum number of ratings.

--- movie_lib.py
+++ movie_lib.py
@@ -1,22 +1,6 @@
 import csv
-
-
-'''user and ratings
-    user_id and user_id & ratings
-    ratings and movies
-    item_id = movie_id
-        user_id makes ratings
-        user_id in ratings
-        ratings on item_id
-        item_id is movie_id
-    Find all ratings for a movie by id
-    Find the average rating for a movie by id
-    Find the name of a movie by id
-    Find all ratings for a user
-
-    View movies by highest average rating (include min rating)
-    Search for a movie
-'''
+import os
+import sys
 
 movie_dict = {}
 user_dict = {}
@@ -37,13 +21,59 @@ class Movie():
     def __repr__(self):
         return self.__str__()
 
-    def get_movie_title_from_id(self, movie_id):
-        if self.movie_id == movie_id:
-            return self.title
-
+    # def get_movie_title_from_id(self, movie_id):
+    #     if self.movie_id == movie_id:
+    #         return self.title
+    #
     # def get_ratings_for_single_movie(self, movie_dict):
     #     movie_dict[self.movie_id].ratings
     #     return
+
+class Rating():
+    # def __init__(self, user_id, item_id, rating):
+    #     self.user_id = user_id
+    #     self.item_id = item_id
+    #     self.rating = rating
+
+    # def __str__(self):
+    #     return "User ID: {}, Item ID: {}, Rating: {}".format(self.user_id, self.item_id, self.rating)
+    #
+    # def __repr__(self):
+    #     return self.__str__()
+
+    def calc_avg_rating(ratings_attr):
+        #should work to calc ratings for a user and for ratings for a movie
+        # all_ratings = [dict.values() for dict in ratings_attr.values()]
+        return round(float(sum(ratings_attr.values())/len(ratings_attr.values())), 1)
+
+    def get_top_rated_movies(num_of_movies, min_num_ratings):
+
+        top_rated_movies = [movie for movie in movie_dict.values() if len(movie.ratings) >= min_num_ratings]
+        return sorted(top_rated_movies, key= lambda movie: movie.avg_rating, reverse=True)[:num_of_movies]
+        # top_rated_movies = []
+        # for movie in movie_dict.values():
+        #     if len(movie.ratings) >= min_num_ratings:
+        #         top_rated_movies.append(movie)
+        #
+        # return sorted(top_rated_movies, key= lambda movie: movie.avg_rating, reverse=True)[:num_of_movies]
+
+    def euclidean_distance(v, w):
+        """Given two lists, give the Euclidean distance between them on a scale
+        of 0 to 1. 1 means the two lists are identical.
+        """
+
+        # Guard against empty lists.
+        if len(v) is 0:
+            return 0
+
+        # Note that this is the same as vector subtraction.
+        differences = [v[idx] - w[idx] for idx in range(len(v))]
+        squares = [diff ** 2 for diff in differences]
+        sum_of_squares = sum(squares)
+
+        return 1 / (1 + math.sqrt(sum_of_squares))
+
+
 
 
 class User():
@@ -63,23 +93,42 @@ class User():
         return self.ratings.values()
 
 
-class Rating():
-    def __init__(self, user_id, item_id, rating):
-        self.user_id = user_id
-        self.item_id = item_id
-        self.rating = rating
+class Interface():
 
-    def __str__(self):
-        return "User ID: {}, Item ID: {}, Rating: {}".format(self.user_id, self.item_id, self.rating)
+    def clear():
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
 
-    def __repr__(self):
-        return self.__str__()
+    def play_again():
+        """Asks user if they want to play the game again."""
 
-    def calc_avg_rating(ratings_attr):
-        #should work to calc ratings for a user and for ratings for a movie
-        # all_ratings = [dict.values() for dict in ratings_attr.values()]
-        return round(float(sum(ratings_attr.values())/len(ratings_attr.values())), 2)
+        play_again = input("Do you want to play again? [y/N] \n")
 
+        if play_again.lower().strip() == "y":
+            clear()
+            main()
+
+        else:
+            print("Thank you. We appreciate your business.")
+            sys.exit()
+
+    def view_top_rated_movies():
+        while True:
+                try:
+                    num_of_movies = int(input("How many movies would you like to view? "))
+                    min_num_ratings = int(input("What's the minimum number of ratings? "))
+
+                except ValueError:
+                    print("That's not a number!\n")
+
+                else:
+                    Interface.clear()
+                    print("RANK\tRATING\tTITLE")
+                    for i, movie in enumerate(Rating.get_top_rated_movies(num_of_movies, min_num_ratings), start=1):
+                        print(i, "\t", movie.avg_rating, "\t", movie.movie_title)
+                    print()
 
 
 def main():
@@ -100,11 +149,9 @@ def main():
         reader = csv.reader(f, delimiter='\t')
         for row in reader:
             #find user ID, add dict of item_id key dict rating
-            # user_dict[int(row[0])].ratings[int(row[1])] = {'rating': int(row[2])}
             user_dict[int(row[0])].ratings[int(row[1])] = int(row[2])
 
             #find movie_id, add dict of user_id key dict rating
-            # movie_dict[int(row[1])].ratings[int(row[0])] = {'rating': int(row[2])}
             movie_dict[int(row[1])].ratings[int(row[0])] = int(row[2])
 
 
@@ -114,54 +161,25 @@ def main():
     for user_obj in user_dict.values():
         user_obj.avg_rating = Rating.calc_avg_rating(user_obj.ratings)
 
-    # def average_movie_rating(movie_dict):
-    #     for movie_object in movie_dict.values():
-    #         movie.avg_rating = Ratings.ave_rating(movie.ratings)
+
+    Interface.view_top_rated_movies()
+
+
+    # """Find all ratings for a movie by id"""
+    # print(list(movie_dict[1].ratings.values()))
+    #
+    # """Find the average rating for a movie by id"""
+    # print(movie_dict[1].avg_rating)
+    #
+    # """Find the name of a movie by id"""
+    # print(movie_dict[1].movie_title)
+    #
+    # """Find all ratings for a user"""
+    # print(user_dict[1].ratings)
 
 
 
-
-
-    print(movie_dict[1])
-
-
-
-
-def euclidean_distance(v, w):
-    """Given two lists, give the Euclidean distance between them on a scale
-    of 0 to 1. 1 means the two lists are identical.
-    """
-
-    # Guard against empty lists.
-    if len(v) is 0:
-        return 0
-
-    # Note that this is the same as vector subtraction.
-    differences = [v[idx] - w[idx] for idx in range(len(v))]
-    squares = [diff ** 2 for diff in differences]
-    sum_of_squares = sum(squares)
-
-    return 1 / (1 + math.sqrt(sum_of_squares))
 
 
 if __name__ == '__main__':
     main()
-
-
-
-# with open('ml-100k/u.user') as f:
-#     reader = csv.DictReader(f, fieldnames=['user_id'], delimiter='|')
-#     for row in reader:
-#         user_list.append(User(int(row['user_id'])))
-
-
-# with open('ml-100k/u.data') as f:
-#     reader = csv.DictReader(f, fieldnames=['user_id', 'item_id', 'rating'], delimiter='\t')
-#     for row in reader:
-#         Rating(int(row['user_id']), int(row['item_id']), int(row['rating']))
-
-
-# with open('ml-100k/u.item', encoding='latin_1') as f:
-#     reader = csv.DictReader(f, fieldnames=['movie_id', 'movie_title',], delimiter='|')
-#     for row in reader:
-#         movie_list.append(Movie(int(row['movie_id']), row['movie_title']))
